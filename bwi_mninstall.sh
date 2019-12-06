e726f25f4b3c301c9dbc4c57135b88d3d48f49d2
mrx0rhk <57437329+mrx0rhk@users.noreply.github.com>
2019-12-06 10:44:09 +0100

    Update bwi_mninstall.sh

--- bwi_mninstall.sh
+++ bwi_mninstall.sh
@@ -97,21 +97,21 @@ then
 else
    echo -e "${GREEN}Updating system and installing required packages. This can take a few minutes...${NC}"
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get update -y 2>/dev/null  >/dev/null 
-sudo apt-get -y upgrade 2>/dev/null  >/dev/null 
-sudo apt-get -y dist-upgrade 2>/dev/null  >/dev/null
-sudo apt-get -y autoremove 2>/dev/null  >/dev/null
-sudo apt-get -y install wget nano htop jq 2>/dev/null  >/dev/null
-sudo apt-get -y install libzmq3-dev 2>/dev/null  >/dev/null
-sudo apt-get -y install libevent-dev -y 2>/dev/null  >/dev/null
-sudo apt-get install unzip 2>/dev/null  >/dev/null
-sudo apt install unzip 2>/dev/null  >/dev/null
-sudo apt -y install software-properties-common 2>/dev/null  >/dev/null
-sudo add-apt-repository ppa:bitcoin/bitcoin -y 2>/dev/null  >/dev/null
-sudo apt-get -y update 2>/dev/null  >/dev/null
-sudo apt-get -y install libdb4.8-dev libdb4.8++-dev -y 2>/dev/null  >/dev/null
-sudo apt-get -y install libminiupnpc-dev 2>/dev/null  >/dev/null
-sudo apt-get install -y unzip libzmq3-dev build-essential libssl-dev libboost-all-dev libqrencode-dev libminiupnpc-dev libboost-system1.58.0 libboost1.58-all-dev libdb4.8++ libdb4.8 libdb4.8-dev libdb4.8++-dev libevent-pthreads-2.0-5 -y 2>/dev/null  >/dev/null 
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y 2>/dev/null 
+sudo apt-get -y upgrade 2>/dev/null
+sudo apt-get -y dist-upgrade 2>/dev/null
+sudo apt-get -y autoremove 2>/dev/null
+sudo apt-get -y install wget nano htop jq 2>/dev/null
+sudo apt-get -y install libzmq3-dev 2>/dev/null
+sudo apt-get -y install libevent-dev -y 2>/dev/null
+sudo apt-get install unzip 2>/dev/null
+sudo apt install unzip 2>/dev/null
+sudo apt -y install software-properties-common 2>/dev/null
+sudo add-apt-repository ppa:bitcoin/bitcoin -y 2>/dev/null
+sudo apt-get -y update 2>/dev/null
+sudo apt-get -y install libdb4.8-dev libdb4.8++-dev -y 2>/dev/null
+sudo apt-get -y install libminiupnpc-dev 2>/dev/null
+sudo apt-get install -y unzip libzmq3-dev build-essential libssl-dev libboost-all-dev libqrencode-dev libminiupnpc-dev libboost-system1.58.0 libboost1.58-all-dev libdb4.8++ libdb4.8 libdb4.8-dev libdb4.8++-dev libevent-pthreads-2.0-5 -y 2>/dev/null 
    fi
    
 
