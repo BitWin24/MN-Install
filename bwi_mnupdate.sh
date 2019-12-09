d12b774072093a8a99ea29e1f4a07bb86ee01edd
mrx0rhk <57437329+mrx0rhk@users.noreply.github.com>
2019-12-09 20:32:04 +0100

    Update bwi_mnupdate.sh

--- bwi_mnupdate.sh
+++ bwi_mnupdate.sh
@@ -70,8 +70,6 @@ clear
 cd ~
 rm -rf /bitwin24-1.0.0
 rm -rf /usr/local/bin/bitwin24*
-cd .bitwin24/
-rm -rf backups/ budget.dat db.log .lock mnpayments.dat chainstate debug.log peers.dat zerocoin blocks database fee_estimates.dat mncache.dat sporks
 wget https://github.com/BitWin24/bitwin24/releases/download/v0.0.6/bitwin24-1.0.0-x86_64-linux-gnu.tar.gz
 tar -xzvf bitwin24-1.0.0-x86_64-linux-gnu.tar.gz
 cd /root/bitwin24-1.0.0/bin/  2>/dev/null  >/dev/null
