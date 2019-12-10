9e3883ffb99574ae50d9f8be9c9de5625d727aa3
mrx0rhk <57437329+mrx0rhk@users.noreply.github.com>
2019-12-10 14:01:56 +0100

    Update bwi_mnupdate.sh

--- bwi_mnupdate.sh
+++ bwi_mnupdate.sh
@@ -63,6 +63,11 @@ read DOSETUP
 if [[ $DOSETUP =~ "n" ]] ; then
           exit 1
     fi
+    
+killall bitwin24d 2>/dev/null  >/dev/null
+sleep 6
+killall bitwin24d 2>/dev/null  >/dev/null
+killall bitwin24d 2>/dev/null  >/dev/null
 
 sleep .5
 clear
@@ -100,6 +105,8 @@ then start the bitwin24d daemon back up:
 to stop:              ${GREEN}bitwin24-cli stop${NC}
 to start:             ${GREEN}bitwin24d${NC}
 to edit:              ${GREEN}nano ~/.bitwin24/bitwin24.conf ${NC}
+to check status:      ${GREEN}bitwin24-cli getinfo ${NC}
+to check MN status:   ${GREEN}bitwin24-cli masternode status ${NC}
 ========================================================================
 To monitor system resource utilization and running processes:
                    ${GREEN}htop${NC}
