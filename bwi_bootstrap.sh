3d5ac94b718a1c84ae21e56a3039e4034a4ad5f2
mrx0rhk <57437329+mrx0rhk@users.noreply.github.com>
2020-02-18 11:52:18 +0100

    Update bwi_bootstrap.sh

--- bwi_bootstrap.sh
+++ bwi_bootstrap.sh
@@ -32,8 +32,8 @@ sleep 5
 # downloading bootstrap
 
 cd ~/.bitwin24/ && rm -rf blocks chainstate sporks zerocoin peers.dat
-cd ~/.bitwin24/ && wget https://www.dropbox.com/s/mg606h8lqgwqk5m/bootstrap.zip
-cd ~/.bitwin24/ && unzip bootstrap.zip
+cd ~/.bitwin24/ && wget https://www.dropbox.com/s/e11ji0e1x7h1jlc/20200218_bwi_bootstrap_152385.zip
+cd ~/.bitwin24/ && unzip 20200218_bwi_bootstrap_152385.zip
 
 
 # starting wallet
@@ -54,4 +54,4 @@ ${GREEN}bitwin24-cli getinfo${NC}
 ========================================================================"
 
 cd ~ && rm -rf bwi_bootstrap.sh 
-cd ~/.bitwin24/ && rm bootstrap.zip
+cd ~/.bitwin24/ && rm 20200218_bwi_bootstrap_152385.zip
