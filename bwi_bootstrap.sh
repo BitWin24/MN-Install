67bc3111c73c06c1425826901fc379fdd38e5d06
mrx0rhk <57437329+mrx0rhk@users.noreply.github.com>
2020-03-02 10:26:46 +0100

    Update bwi_bootstrap.sh

--- bwi_bootstrap.sh
+++ bwi_bootstrap.sh
@@ -34,10 +34,10 @@ sleep 5
 rm bwi_bootstrap*
 cd ~/.bitwin24/
 rm -rf backups blocks chainstate debug.log .lock mncache.dat peers.dat staking zerocoin banlist.dat budget.dat db.log fee_estimates.dat mnpayments.dat  sporks
-cd ~/.bitwin24/ && wget https://www.dropbox.com/s/e11ji0e1x7h1jlc/20200218_bwi_bootstrap_152385.zip
-cd ~/.bitwin24/ && unzip 20200218_bwi_bootstrap_152385.zip
+cd ~/.bitwin24/ && wget https://www.dropbox.com/s/mg606h8lqgwqk5m/bootstrap.zip
+cd ~/.bitwin24/ && unzip bootstrap.zip
 
-rm 2020*
+rm bootstrap.zip*
 
 
 
@@ -58,4 +58,4 @@ ${GREEN}bitwin24-cli getinfo${NC}
 
 ========================================================================"
 cd ~
-rm bwi_bootstrap*
+rm bwi_bootstrap.sh*
